acef0346df149416844e1672afa447e5efbe8069
Fei Wang <fei.wang@morningstar.com>
2016-02-18 14:48:11 +0800

    Adds database Create, Read, Update, and Delete support with custom dialogs

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -17,10 +17,16 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.SQLException;
@@ -59,22 +65,8 @@ public class RemindersActivity extends AppCompatActivity {
         int[] to = new int[]{R.id.row_text};
         mCursorAdapter = new RemindersSimpleCursorAdapter(RemindersActivity.this, R.layout.reminders_row, cursor, from, to, 0);
         mListView.setAdapter(mCursorAdapter);
-        mDbAdapter.close();
 
-//        //The arrayAdatper is the controller in our
-//        //model-view-controller relationship. (controller)
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-//                //context
-//                this,
-//                //layout (view)
-//                R.layout.reminders_row,
-//                //row (view)
-//                R.id.row_text,
-//                //data (model) with bogus data to test our listview
-//                new String[]{"first record", "second record", "third record", "fourth record", "fifth record"});
-//        mListView.setAdapter(arrayAdapter);
-
-
+        //testStaticData();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -105,12 +97,13 @@ public class RemindersActivity extends AppCompatActivity {
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         //edit reminder
                         if (position == 0) {
-                            Toast.makeText(RemindersActivity.this, "edit "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
+                            int nId = getIdFromPosition(masterListPosition);
+                            Reminder reminder = mDbAdapter.fetchReminderById(nId);
+                            fireCustomDialog(reminder);
                         //delete reminder
                         } else {
-                            Toast.makeText(RemindersActivity.this, "delete "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
+                            mDbAdapter.deleteReminderById(getIdFromPosition(masterListPosition));
+                            mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
                         }
                         dialog.dismiss();
                     }
@@ -162,6 +155,20 @@ public class RemindersActivity extends AppCompatActivity {
 
     }
 
+    private void testStaticData() {
+        //The arrayAdatper is the controller in our model-view-controller relationship. (controller)
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                //context
+                this,
+                //layout (view)
+                R.layout.reminders_row,
+                //row (view)
+                R.id.row_text,
+                //data (model) with bogus data to test our listview
+                new String[]{"first record", "second record", "third record", "fourth record", "fifth record"});
+        mListView.setAdapter(arrayAdapter);
+    }
+
     private int getIdFromPosition(int nC) {
         return (int)mCursorAdapter.getItemId(nC);
     }
@@ -185,6 +192,50 @@ public class RemindersActivity extends AppCompatActivity {
         mDbAdapter.createReminder("Call the Dalai Lama back", true);
     }
 
+    private void fireCustomDialog(final Reminder reminder){
+// custom dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_custom);
+        TextView titleView = (TextView) dialog.findViewById(R.id.custom_title);
+        final EditText editCustom = (EditText) dialog.findViewById(R.id.custom_edit_reminder);
+        Button commitButton = (Button) dialog.findViewById(R.id.custom_button_commit);
+        final CheckBox checkBox = (CheckBox) dialog.findViewById(R.id.custom_check_box);
+        LinearLayout rootLayout = (LinearLayout) dialog.findViewById(R.id.custom_root_layout);
+        final boolean isEditOperation = (reminder != null);
+//this is for an edit
+        if (isEditOperation){
+            titleView.setText("Edit Reminder");
+            checkBox.setChecked(reminder.getImportant() == 1);
+            editCustom.setText(reminder.getContent());
+            rootLayout.setBackgroundColor(getResources().getColor(R.color.blue));
+        }
+        commitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String reminderText = editCustom.getText().toString();
+                if (isEditOperation) {
+                    Reminder reminderEdited = new Reminder(reminder.getId(),
+                            reminderText, checkBox.isChecked() ? 1 : 0);
+                    mDbAdapter.updateReminder(reminderEdited);
+//this is for new reminder
+                } else {
+                    mDbAdapter.createReminder(reminderText, checkBox.isChecked());
+                }
+                mCursorAdapter.changeCursor(mDbAdapter.fetchAllReminders());
+                dialog.dismiss();
+            }
+        });
+        Button buttonCancel = (Button) dialog.findViewById(R.id.custom_button_cancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -197,6 +248,7 @@ public class RemindersActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_new:
                 Log.d(getLocalClassName(), "create new Reminder");
+                fireCustomDialog(null);
                 return true;
             case R.id.action_exit:
                 Log.d(getLocalClassName(), "exit App");
