9d0359902229ad86fa1ab1efd64b9be3a19a0f4f
Fei Wang <fei.wang@morningstar.com>
2016-02-18 16:29:12 +0800

    Adds new scheduled time picker option

---
+++ app/src/main/java/com/apress/gerber/reminders/ReminderAlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.apress.gerber.reminders;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by fwang5 on 2/18/2016.
+ */
+public class ReminderAlarmReceiver extends BroadcastReceiver {
+
+    public static final String REMINDER_TEXT = "REMINDER_TEXT";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
